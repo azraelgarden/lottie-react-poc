f490d5ef728e754ef40b6c830ac644e21b2c1663
Azrael Garden <azraelgarden@gmail.com>
2021-10-20 10:54:48 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyle from './global.styles';
+
+function App(): JSX.Element {
+    return (
+        <>
+            <GlobalStyle />
+        </>
+    );
+}
+
+export default App;
